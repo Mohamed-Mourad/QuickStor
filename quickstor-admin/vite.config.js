6706435e3d0a155ee5a94bc64e388d6ba3f50d0e
Mohamed Mourad <67578210+Mohamed-Mourad@users.noreply.github.com>
2025-12-30 10:16:30 +0200

    Update vite.config.js

--- quickstor-admin/vite.config.js
+++ quickstor-admin/vite.config.js
@@ -9,10 +9,6 @@ export default defineConfig({
     alias: {
       react: path.resolve('./node_modules/react'),
       'react-dom': path.resolve('./node_modules/react-dom'),
-      recharts: path.resolve('./node_modules/recharts'),
-      'lucide-react': path.resolve('./node_modules/lucide-react'),
-      clsx: path.resolve('./node_modules/clsx'),
-      'tailwind-merge': path.resolve('./node_modules/tailwind-merge'),
     },
   },
 })
